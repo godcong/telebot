803a1f8752b16b51a22d95b676a647349b918315
godcong <jumbycc@163.com>
2019-05-10 16:31:56 +0800

    update yinhe

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"github.com/girlvr/yinhe_bot/message"
+	api "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/sirupsen/logrus"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
@@ -13,4 +19,60 @@ func main() {
 	logrus.Info(string(token))
 	logrus.SetReportCaller(true)
 	BootWithGAE(string(token))
+}
+
+// BootWithGAE ...
+func BootWithGAE(token string) {
+	bot, err := api.NewBotAPI(token)
+	if err != nil {
+		log.Fatal(err)
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+	t := "crVuYHQbUWCerib3"
+	_, err = bot.SetWebhook(api.NewWebhook("https://bot.dhash.app/" + t))
+	if err != nil {
+		log.Fatal(err)
+	}
+	info, err := bot.GetWebhookInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if info.LastErrorDate != 0 {
+		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
+	}
+
+	updates := bot.ListenForWebhook("/" + t)
+	go http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	message.InitBoot(bot)
+	for update := range updates {
+		message.HookMessage(update)
+	}
+}
+
+// BootWithUpdate ...
+func BootWithUpdate(token string) {
+	bot, err := api.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := api.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		message.HookMessage(update)
+	}
 }
