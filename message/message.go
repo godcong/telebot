f618617b24e39c3b954c63853b3a7d40481e6a82
godcong <jumbycc@gmail.com>
2019-05-22 13:42:46 +0800

    update yinhe

--- message/message.go
+++ message/message.go
@@ -139,6 +139,7 @@ func HookMessage(update tgbotapi.Update) {
 /v 或 /video +番号 查询视频 如：/v ssni-334
 /t 或 /top　显示推荐视频
 /l 或 /list 显示列表（仅私聊）
+/d 或 /down 获取求哈希下载链接
 /h 或 /help 显示帮助
 `
 		cts = append(cts, tgbotapi.NewMessage(update.Message.Chat.ID, help))
@@ -148,6 +149,8 @@ func HookMessage(update tgbotapi.Update) {
 		closeMsg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 		closeMsg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
 		cts = append(cts, closeMsg)
+	case "down", "d":
+		cts = append(cts, tgbotapi.NewMessage(update.Message.Chat.ID, "请认准官方下载地址：xxx"))
 	default:
 		return
 	}
