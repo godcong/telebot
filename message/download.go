f1296174f5664776ed5f159ec26cefbca083b6d8
godcong <jumbycc@163.com>
2019-07-26 16:51:31 +0800

    update yinhe

--- message/download.go
+++ message/download.go
@@ -6,10 +6,7 @@ import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 func Download(message *tgbotapi.Message) (able tgbotapi.Chattable) {
 	log.With("id", downloadFileID).Info("download")
 	if downloadFileID == "" {
-		down := tgbotapi.NewDocumentUpload(message.Chat.ID, GetProperty().Download)
-		downloadFileID = down.FileID
-		return down
+		return tgbotapi.NewDocumentUpload(message.Chat.ID, GetProperty().Download)
 	}
-	able = tgbotapi.NewDocumentUpload(message.Chat.ID, downloadFileID)
-	return
+	return tgbotapi.NewDocumentUpload(message.Chat.ID, downloadFileID)
 }
