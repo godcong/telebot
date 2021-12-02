7dcec8f34927c5f9d8fbc31f86ce2d475961b811
godcong <jumbycc@163.com>
2021-12-02 22:32:10 +0800

    feat(bot): create new feat

--- pkg/sync.sh
+++ pkg/sync.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+echo "Clone required repositories"
 git clone https://github.com/zelenin/go-tdlib.git
 git clone https://github.com/tdlib/td.git
 
-cp -f ./backup/client/tdjson_dynamic.go ./go-tdlib/td/client/
-cp -f ./backup/client/tdlib.go ./go-tdlib/td/client/
+echo "Copying files..."
+cp -f ./backup/client/tdjson_dynamic.go ./go-tdlib/client/
+cp -f ./backup/client/tdlib.go ./go-tdlib/client/
+
+echo "Remove go.mod"
 rm ./go-tdlib/go.mod
